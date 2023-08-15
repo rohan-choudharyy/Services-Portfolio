7daf0eea3562cb202e54ea920e40e66511d266a8
CodeSandbox <support@codesandbox.io>
2023-08-15 06:06:13 +0000

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>RC Solutions</h1>
+                <p>@all rights reserved</p>
+            </div>
+
+            <div>
+                <h5>Follow Us</h5>
+                <div>
+                    <a href="https://www.linkedin.com/in/rohanchoudharyy/" target={"blanck"}>LinkedIn</a>
+                    <a href="https://twitter.com/Rohan011102" target={"blanck"}>Twitter</a>
+                </div>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
